41ba9f265031081975844f74816d14e0fff05436
Zakaria-Boukeffa <zakaria.boukeffa.01@gmail.com>
2025-11-01 18:17:39 +0100

    Weather API initialised, GG

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello</h1>'
+load_dotenv()
+app.config["OPENWEATHER_API_KEY"] = os.getenv("OPENWEATHER_API_KEY")
+
+from routes import *
 
 if __name__ == '__main__':
-      app.run(debug=True)
+    app.run(debug=True)
